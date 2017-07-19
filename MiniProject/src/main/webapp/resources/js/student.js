28bbbfc4c39ad6d208994730f7277afd69072534
JaydeepBhatia <jaydeep.bhatiamain@gmail.com>
2017-07-19 14:57:57 -0400

    miniproject

---
+++ MiniProject/src/main/webapp/resources/js/student.js
@@ -0,0 +1,38 @@
+/**
+ *  Student's code
+ */
+
+
+window.onload = function(){
+	setListeners();
+	
+}
+
+
+function setListeners(){
+	
+document.getElementById('info').addEventListener('click', getInfo, false);	
+console.log('I was here too');
+
+}
+
+function getInfo(){
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function() {
+		if(xhr.readyState == 4 && xhr.status ==200){
+			console.log(xhr.responseText);
+			var studentInfo = JSON.parse(xhr.responseText);
+			document.getElementById('userid').innerHTML = studentInfo.userId;
+			document.getElementById('email').innerHTML = studentInfo.email;
+			document.getElementById('firstname').innerHTML = studentInfo.firstName;
+			document.getElementById('lastname').innerHTML = studentInfo.lastName;
+			
+		
+		}
+			
+		}
+	
+	xhr.open("GET", "userinfo", true);
+	xhr.send();
+	
+}
